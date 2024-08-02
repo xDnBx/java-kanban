1fae68e79cbeca9cbcc813a092f36cbe4375e94a
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2024-08-02 21:38:20 +0300

    Убрал сеттер для epicTaskId и удалил объект класса TaskManager. Также удалил ненужные импорты.

--- src/tasks/Subtask.java
+++ src/tasks/Subtask.java
@@ -1,13 +1,9 @@
 package tasks;
 
 import enums.TaskStatus;
-import managers.TaskManager;
-
-import java.util.ArrayList;
 
 public class Subtask extends Task{
-    protected int epicTaskId;
-    TaskManager taskManager = new TaskManager();
+    protected final int epicTaskId;
 
     public Subtask(String name, String description, TaskStatus taskStatus, int epicTaskId) {
         super(name, description, taskStatus);
@@ -16,10 +12,6 @@ public class Subtask extends Task{
 
     public int getEpicTaskId() {
         return epicTaskId;
-    }
-
-    public void setEpicTaskId(int epicTaskId) {
-        this.epicTaskId = epicTaskId;
     }
 
     @Override
