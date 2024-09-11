cd0f27d203f3be39b967f7a57478a76873bf246d
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2024-09-11 16:56:27 +0300

    Добавить класс Node, добавить методы в интерфейс HistoryManager и реализовать их в классе

--- src/managers/HistoryManager.java
+++ src/managers/HistoryManager.java
@@ -8,5 +8,13 @@ public interface HistoryManager {
 
     void add(Task task);
 
+    void remove(int id);
+
     List<Task> getHistory();
+
+    void removeNode(Node<Task> node);
+
+    Node<Task> linkLast(Task task);
+
+    List<Task> getTasks();
 }
--- src/managers/InMemoryHistoryManager.java
+++ src/managers/InMemoryHistoryManager.java
@@ -3,24 +3,74 @@ package managers;
 import tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    protected final List<Task> historyTask = new ArrayList<>();
+    protected final Map<Integer, Node<Task>> historyTask = new HashMap<>();
+    protected Node<Task> head;
+    protected Node<Task> tail;
 
     @Override
     public void add(Task task) {
-        if (task != null) {
-            historyTask.add(task);
-        }
-        if (historyTask.size() > 10) {
-            historyTask.removeFirst();
+        int taskId = task.getId();
+        remove(taskId);
+        historyTask.put(taskId, linkLast(task));
+    }
+
+    @Override
+    public void remove(int id) {
+        if (historyTask.containsKey(id)) {
+            removeNode(historyTask.get(id));
+            historyTask.remove(id);
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return new ArrayList<>(historyTask);
+        return getTasks();
+    }
+
+    @Override
+    public List<Task> getTasks() {
+        List<Task> tasks = new ArrayList<>();
+        Node<Task> current = head;
+        for (int i = 0; i < historyTask.size(); i++) {
+            tasks.add(current.data);
+            current = current.next;
+        }
+        return tasks;
+    }
+
+    @Override
+    public Node<Task> linkLast(Task task) {
+        Node<Task> updNode = new Node<>(task);
+        if (head == null) {
+            head = updNode;
+        } else {
+            tail.next = updNode;
+            updNode.prev = tail;
+        }
+        tail = updNode;
+        return updNode;
+    }
+
+    @Override
+    public void removeNode(Node<Task> node) {
+        if (node == null) {
+            return;
+        }
+        if (node.prev != null) {
+            node.prev.next = node.next;
+        } else {
+            head = node.next;
+        }
+        if (node.next != null) {
+            node.next.prev = node.prev;
+        } else {
+            tail = node.prev;
+        }
     }
 }
