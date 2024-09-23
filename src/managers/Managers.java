a420254d3a1aa11888f4237a1ca8ee0529cf573a
Oleg Kitaev <oleg.kitaev61@yandex.ru>
2024-09-23 13:27:30 +0300

    Исправить название константы

--- src/managers/Managers.java
+++ src/managers/Managers.java
@@ -3,10 +3,10 @@ package managers;
 import java.io.File;
 
 public class Managers {
-    public static final File fileCsv = new File("data.csv");
+    public static final File FILE_CSV = new File("data.csv");
 
     public static TaskManager getDefault() {
-        return new FileBackedTaskManager(fileCsv);
+        return new FileBackedTaskManager(FILE_CSV);
     }
 
     public static HistoryManager getDefaultHistory() {
